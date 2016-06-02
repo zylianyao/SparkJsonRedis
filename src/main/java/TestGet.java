d49c67ac4ad75d0ccda27ca44d49a9ae3bce0275
wshfzhwj <wshfzhwj@126.com>
2016-06-02 11:42:54 +0800

    测试Spark并发

---
+++ src/main/java/TestGet.java
@@ -0,0 +1,49 @@
+/**
+ * 拿客 www.coderknock.com
+ * 微信公众号 coderknock
+ * 作者：三产
+ */
+
+import jodd.http.HttpRequest;
+import jodd.http.HttpResponse;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+public class TestGet {
+    private static int thread_num = 500;
+    private static int client_num = 500;
+
+    public static void main(String[] args) {
+        long time = System.currentTimeMillis();
+        ExecutorService exec = Executors.newCachedThreadPool();
+        final Semaphore semp = new Semaphore(thread_num);
+        for (int index = 0; index < client_num; index++) {
+            final int NO = index;
+            Runnable run = new Runnable() {
+                public void run() {
+                    try {
+                        semp.acquire();
+                        System.out.println("------------------|" + NO + "|------------------");
+                        HttpResponse response = HttpRequest
+                                .get("http://localhost:4567/say/Hello-" + NO + "/to/World" + NO
+                                )
+                                .acceptEncoding("gzip")
+                                .send();
+
+                        System.out.println(response.unzip());
+                        System.out.println("------------------&" + NO + "&------------------");
+                        //业务逻辑
+                        semp.release();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+            exec.execute(run);
+        }
+        System.out.println("------------------" + (System.currentTimeMillis() - time) + "------------------");
+        exec.shutdown();
+    }
+}
